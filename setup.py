afcdbc9ce87ccc86c96c07dac42879e5b835d4a2
muzahid120 <muzahid771@gmail.com>
2024-03-31 13:09:53 +0530

    EDA

--- setup.py
+++ setup.py
@@ -1,1 +1,26 @@
-print('Hello World!')
+from setuptools import find_packages, setup
+from typing import List
+
+hypen_dot = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function will return a list of requirements.
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements] 
+        if hypen_dot in requirements:
+            requirements.remove(hypen_dot)
+
+    return requirements
+
+setup(
+    name='ML Project',
+    version='0.0.1',
+    author='Sk Muzahid',
+    author_email='muzahidsk771@gmail.com',  # Fixed email format
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
